7b02dc59f60ad6aac047c2532356966bdc8ea477
knsnedden <kayleah.snedden@trojans.dsu.edu>
2020-11-20 10:46:08 -0800

    implemented generateRandomCostMatrix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[][] arr = Generate.generateRandomCostMatrix(5,10);
+
+	    System.out.println("     |  0  |  1  |  2  |  3  |  4  |");
+	    for (int i = 0; i < 5; ++i){
+	        System.out.printf("  %d  |", i);
+	        for (int j = 0; j < 5; ++j){
+	            System.out.printf("  %d  |", arr[i][j]);
+            }
+	        System.out.println();
+        }
     }
+
+
 }
