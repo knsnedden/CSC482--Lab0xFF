294743e1d5ff125a044d679bc3058d257563f35e
knsnedden <kayleah.snedden@trojans.dsu.edu>
2020-11-23 12:32:40 -0800

    implemented generateRandomEuclideanCostMatrix

--- src/com/company/Generate.java
+++ src/com/company/Generate.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Random;
+import java.lang.Math;
 
 public class Generate {
     public static int[][] generateRandomCostMatrix(int size, int maxCost){
@@ -23,5 +24,34 @@ public class Generate {
         }
 
         return arr;
+    }
+
+    public static float[][] generateRandomEuclideanCostMatrix(int size, int maxX, int maxY){
+        float[][] cost = new float[size][size];
+        int[][] coord = new int[size][2];
+        Random newRand = new Random();
+        int rand;
+        float result;
+
+        // create random coordinates
+        for (int i = 0; i < size; ++i){
+            rand = newRand.nextInt(maxX);
+            coord[i][0] = rand;
+            rand = newRand.nextInt(maxY);
+            coord[i][1] = rand;
+        }
+
+        // calculate the costs
+        for (int i = 0; i < size; ++i){
+            for (int j = i; j < size; ++j){
+                double x = Math.pow(coord[i][0] - coord[j][0],2);
+                double y = Math.pow(coord[i][1] - coord[j][1],2);
+                result = (float)(Math.sqrt(x+y));
+                cost[i][j] = result;
+                cost[j][i] = result;
+            }
+        }
+
+        return cost;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    int[][] arr = Generate.generateRandomCostMatrix(5,10);
+	    float[][] arr = Generate.generateRandomEuclideanCostMatrix(5,10, 10);
 
-	    System.out.println("     |  0  |  1  |  2  |  3  |  4  |");
+	    System.out.println("     |   0    |   1    |   2    |   3    |   4    |");
 	    for (int i = 0; i < 5; ++i){
 	        System.out.printf("  %d  |", i);
 	        for (int j = 0; j < 5; ++j){
-	            System.out.printf("  %d  |", arr[i][j]);
+	            System.out.printf("  %.2f  |", arr[i][j]);
             }
 	        System.out.println();
         }
