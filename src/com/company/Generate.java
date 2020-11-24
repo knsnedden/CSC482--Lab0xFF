e1f665bbe280a4cff453690465adadaf320ad63d
knsnedden <kayleah.snedden@trojans.dsu.edu>
2020-11-23 20:08:45 -0800

    implemented generateRandomCircularGraphCostMatrix

--- src/com/company/Generate.java
+++ src/com/company/Generate.java
@@ -1,7 +1,10 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.lang.Math;
+import java.util.Collections;
 
 public class Generate {
     public static int[][] generateRandomCostMatrix(int size, int maxCost){
@@ -42,6 +45,41 @@ public class Generate {
         }
 
         // calculate the costs
+        for (int i = 0; i < size; ++i){
+            for (int j = i; j < size; ++j){
+                double x = Math.pow(coord[i][0] - coord[j][0],2);
+                double y = Math.pow(coord[i][1] - coord[j][1],2);
+                result = (float)(Math.sqrt(x+y));
+                cost[i][j] = result;
+                cost[j][i] = result;
+            }
+        }
+
+        return cost;
+    }
+
+    public static float[][] generateRandomCircularGraphCostMatrix(int size, int radius){
+        float[][] cost = new float[size][size];
+        float[][] coord = new float[size][2];
+        Integer[] vertices = new Integer[size];
+        float result;
+
+        // fill vertices array and then shuffle points
+        for (int i = 0; i < size; ++i){
+            vertices[i] = i;
+        }
+        List<Integer> hold = Arrays.asList(vertices);
+        Collections.shuffle(hold.subList(1, hold.size()));
+        hold.toArray(vertices);
+
+        // populate coordinates
+        double stepAngle = (Math.PI * 2)/size;
+        for (int i = 0; i < size; ++i){
+            coord[vertices[i]][0] = (float)(radius * Math.sin(i * stepAngle));
+            coord[vertices[i]][1] = (float)(radius * Math.cos(i * stepAngle));
+        }
+
+        // calculate cost
         for (int i = 0; i < size; ++i){
             for (int j = i; j < size; ++j){
                 double x = Math.pow(coord[i][0] - coord[j][0],2);
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    float[][] arr = Generate.generateRandomEuclideanCostMatrix(5,10, 10);
+	    float[][] arr = Generate.generateRandomCircularGraphCostMatrix(6,10);
 
-	    System.out.println("     |   0    |   1    |   2    |   3    |   4    |");
-	    for (int i = 0; i < 5; ++i){
+	    System.out.println("     |   0    |   1    |   2    |   3    |   4    |   5    |");
+	    for (int i = 0; i < 6; ++i){
 	        System.out.printf("  %d  |", i);
-	        for (int j = 0; j < 5; ++j){
+	        for (int j = 0; j < 6; ++j){
 	            System.out.printf("  %.2f  |", arr[i][j]);
             }
 	        System.out.println();
