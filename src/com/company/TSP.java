9d894706493bea6d8cb22744f83e62934a41d7c3
knsnedden <kayleah.snedden@trojans.dsu.edu>
2020-12-04 09:52:29 -0800

    solution quality implemented

--- src/com/company/TSP.java
+++ src/com/company/TSP.java
@@ -2,15 +2,19 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 
 public class TSP {
 
     // O(N^2*log2(N))
+    public static float greedySum;
     public static int[] TspGreedy(float[][] arr, int n){
         int[] result = new int[n+1];
         int counter = 1, i = 0, j = 0;
         float min = Integer.MAX_VALUE;
         List<Integer> visited = new ArrayList<>();
+        greedySum = 0;
 
         visited.add(0); // visit home node
         result[0] = 0;
@@ -31,6 +35,7 @@ public class TSP {
             ++j;
             // look at all paths from i
             if (j == n){
+                greedySum += min;
                 min = Integer.MAX_VALUE;
                 visited.add(result[counter]);
                 j = 0;
@@ -48,6 +53,10 @@ public class TSP {
                 result[counter] = j;
             }
         }
+        greedySum += min;
+
+        greedySum += arr[result[n-1]][0];
+
 
         return result;
     }
@@ -107,7 +116,7 @@ public class TSP {
     public static ArrayList<int[]> permutations = new ArrayList<int[]>();
     public static float bruteSum = 0;
 
-    //O(n!)
+    //O(n-1!)
     public static int[] TspBrute(float[][] arr, int size){
         bruteSum = 0;
         int[] bestTour = new int[size];
@@ -194,6 +203,119 @@ public class TSP {
             System.out.printf("  Cost of path: %.2f\n\n", bruteSum);
 
         }
+    }
+
+    public static void performanceTesting(){
+        int N = 4, prev_N = 0;
+        boolean keepGoing = true;
+        long bruteTime = 0, prevBrute = 0, greedyTime = 0, prevGreedy = 0, maxTime = (long)Math.pow(2,27);
+        long timeBefore = 0, timeAfter = 0;
+        float ratio = 0, eRatio = 0;
+
+        System.out.println("         Brute Force                             Greedy");
+        System.out.println("  N  |   Time   |   Inc. Ratio  |  Exp. Ratio  |   Time   |  Inc. Ratio  | Exp. Ratio |");
+
+        while (keepGoing){
+            System.out.printf("%4d |", N);
+            float[][] arr = Generate.generateRandomEuclideanCostMatrix(N, 100,100);
+
+            if (bruteTime <= maxTime){
+                timeBefore = getCpuTime();
+                TspBrute(arr, N);
+                timeAfter = getCpuTime();
+                bruteTime = timeAfter - timeBefore;
+                System.out.printf("%9d |", bruteTime);
+                if (prev_N == 0){
+                    System.out.printf("      na       |       na     |");
+                }else{
+                    ratio = (float)(bruteTime/prevBrute);
+                    eRatio = (float)(factorial(N-1)/factorial(prev_N-1));
+                    System.out.printf("%14.2f | %12.2f |",ratio,eRatio);
+                }
+                prevBrute = bruteTime;
+            }else{
+                System.out.printf("   --     |      --       |      --      |");
+            }
+
+            if (greedyTime <= maxTime){
+                timeBefore = getCpuTime();
+                TspGreedy(arr, N);
+                timeAfter = getCpuTime();
+                greedyTime = timeAfter - timeBefore;
+                System.out.printf("%9d |", greedyTime);
+                if (prev_N == 0){
+                    System.out.printf("      na       |       na     |");
+                }else{
+                    ratio = (float)(greedyTime/prevGreedy);
+                    eRatio = (float)((Math.pow(N,2) * (Math.log(N)/Math.log(2)))/(Math.pow(prev_N,2) * (Math.log(prev_N)/Math.log(2))));
+                    System.out.printf("%14.2f | %12.2f |",ratio,eRatio);
+                }
+                prevGreedy = greedyTime;
+            }else{
+                System.out.printf("   --     |      --       |      --      |");
+            }
+
+            if (bruteTime >= maxTime && greedyTime >= maxTime){
+                keepGoing = false;
+            }
+            prev_N = N;
+            if (bruteTime >= maxTime){
+                N = N*2;
+            }else{
+                N = N+1;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static int factorial(int n){
+        if (n <= 2){
+            return n;
+        }
+        return (n * factorial(n-1));
+    }
+
+    public static void solutionQuality(){
+        int N = 4;
+        long bruteTime = 0, maxTime = (long)Math.pow(2,27);
+        float optimalSolution = 0, allGreedySum = 0, sqr = 0;
+        long timeBefore = 0, timeAfter = 0;
+
+        System.out.println("  N  |  SQR  | Opt. Sol | Greedy Sol |");
+
+        while (bruteTime <= maxTime){
+            System.out.printf("%4d |", N);
+            float[][] bArr = Generate.generateRandomEuclideanCostMatrix(N, 100,100);
+            float[][] gArr = bArr.clone();
+
+            timeBefore = getCpuTime();
+            int[] bruteResult = TspBrute(bArr,N);
+            timeAfter = getCpuTime();
+            bruteTime = timeAfter - timeBefore;
+            optimalSolution = bruteSum;
+
+            int[] greedyResult = new int[N];
+            for (int i = 0; i < 10; ++i){
+                greedyResult = TspGreedy(gArr, N);
+                allGreedySum += greedySum;
+            }
+            allGreedySum = allGreedySum/10;
+
+            sqr = allGreedySum / optimalSolution;
+            System.out.printf("%6.2f |%9.2f | %11.2f", sqr, optimalSolution, allGreedySum);
+
+            N++;
+            allGreedySum = 0;
+            System.out.println();
+        }
+
+    }
+
+    public static long getCpuTime(){
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        return bean.isCurrentThreadCpuTimeSupported() ?
+                bean.getCurrentThreadCpuTime() : 0L;
     }
 
 
